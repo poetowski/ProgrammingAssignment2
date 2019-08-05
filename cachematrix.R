22ffe8a8e4647b2dd9d00f6df47e727709deccd7
poetowski <jakubkomorny@gmail.com>
2019-08-05 23:20:31 +0200

    Update cachematrix.R
    [quest2week3] Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## OBJECT CONTAINING METHODS FOR GET/SET/INVERSE AND MATRIX OBJECT
+makeCacheMatrix <- function(mat = matrix()) {
+  
+  # INITIALIZE LIKE IN TUTORIAL
+  invMat <- NULL
+  
+  # SET METHOD
+  set <- function(new) {
+    mat <<- new
+  }
+  
+  # GET METHOD
+  get <- function() {
+    # LOADING FROM DIFF ENV
+    return(mat)
+  }
+  
+  # SET INVERSION OF MATRIX
+  setInverse <- function(inverse) {
+    ## THIS IS FOR SAVING INTO DIFF ENV
+    invMat <<- inverse
+  }
+  
+  # RETURN CACHED INVERTED MATRIX
+  getInverse <- function() {
+    return(invMat)
+  }
+  
+  methods <- list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  
+  return (methods)
+}
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## RETURN EITHER CACHED INVERSION MATRIX OF THE MATRIX, OR COMPUTE THE NEW ONE
+cacheSolve <- function(mat, ...) {
+  
+  # TRY RESTORE CACHED DATA:
+  inversion <- mat$getInverse()
+  if (!is.null(inversion)) {
+    return(inversion)
+  }
+  
+  # NEW DATA:
+  data <- mat$get()
+  # GET INV OF MAT
+  inversion <- solve(data, ...)
+  # SET IN FOR NEW DATA
+  mat$setInverse(inversion)
+  
+  return(inversion)
 }
 
 
-## Write a short comment describing this function
+## TEST CASES:
+testVector <- c(5,4,3,7,11,23,45,678,211)
+testDim <- sqrt(length(testVector))
+testMatrix <- matrix(testVector,testDim, testDim)
+testMatrixAsParam <- makeCacheMatrix(testMatrix)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## BOTH OBJECTS RETURNED ARE THE SAME, ONE IS NEW, SECOND IS CACHED OLD :)
+beforeCacheProcess <- cacheSolve(testMatrixAsParam) 
+print(beforeCacheProcess)
+afterCacheProcess <- cacheSolve(testMatrixAsParam)
+print(afterCacheProcess)
